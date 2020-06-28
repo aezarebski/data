656a75e7310fb4e44e2c01fde804c7498a5db18b
Alex Zarebski <aezarebski@gmail.com>
2020-06-28 16:11:18 +0100

    add another graph of mobility and style the figure

--- doc/mapping.R
+++ doc/mapping.R
@@ -6,9 +6,13 @@ library(dplyr)
 library(cowplot)
 library(reshape2)
 
-my_breaks <- seq(from = 2000, to = 10000, by = 1000)
+my_breaks <- seq(from = 0, to = 20000, by = 5000)
 
-my_cols <- hcl.colors(n = length(my_breaks) - 1, palette = "viridis", alpha = NULL, rev = FALSE, fixup = TRUE)
+my_cols <- hcl.colors(n = length(my_breaks) - 1, palette = "reds 3", alpha = NULL, rev = TRUE, fixup = TRUE)
+
+map_country_line_colour <- "#FFFFFF"
+
+map_plot_background <- element_rect(colour = "#FFFFFF", fill = "#f0f0f0")
 
 
 italy_sf <- gadm_sf_loadCountries("ITA", level = 1)$sf
@@ -25,39 +29,51 @@ epi_df <- rename(epi_df, NAME_1 = adm_area_1)
 date_index_1 <- "01-04-2020"
 plot_sf_1 <- left_join(italy_sf, filter(epi_df, date == date_index_1), by = "NAME_1")
 g_1 <- ggplot() +
-    geom_sf(data = plot_sf_1, mapping = aes(fill = dead)) +
-    scale_fill_stepsn(breaks = my_breaks,
-                      colors = my_cols,
-                      limits = range(my_breaks))
+    geom_sf(data = plot_sf_1, mapping = aes(fill = dead), colour = map_country_line_colour) +
+    scale_fill_gradientn(breaks = my_breaks,
+                         colors = my_cols,
+                         limits = range(my_breaks)) +
+    theme_nothing() +
+    theme(plot.background = map_plot_background)
 
 
 date_index_2 <- "01-05-2020"
 plot_sf_2 <- left_join(italy_sf, filter(epi_df, date == date_index_2), by = "NAME_1")
 g_2 <- ggplot() +
-    geom_sf(data = plot_sf_2, mapping = aes(fill = dead)) +
-    scale_fill_stepsn(breaks = seq(from = 2000, to = 10000, by = 1000),
+    geom_sf(data = plot_sf_2, mapping = aes(fill = dead), colour = map_country_line_colour) +
+    scale_fill_gradientn(breaks = my_breaks,
                       colors = my_cols,
-                      limits = range(my_breaks))
+                      limits = range(my_breaks)) +
+    theme_nothing() +
+    theme(plot.background = map_plot_background)
 
 
 date_index_3 <- "01-06-2020"
 plot_sf_3 <- left_join(italy_sf, filter(epi_df, date == date_index_3), by = "NAME_1")
 
 g_3 <- ggplot() +
-    geom_sf(data = plot_sf_3, mapping = aes(fill = dead)) +
-    scale_fill_stepsn(breaks = my_breaks,
+    geom_sf(data = plot_sf_3, mapping = aes(fill = dead), colour = map_country_line_colour) +
+    scale_fill_gradientn(breaks = my_breaks,
                       colors = my_cols,
-                      limits = range(my_breaks))
+                      limits = range(my_breaks)) +
+    theme_nothing() +
+    theme(plot.background = map_plot_background)
 
+italy_legend <- get_legend(g_1 +
+                           labs(fill = "Cumulative deaths") +
+                           theme(legend.position = "left",
+                                 legend.box.margin = margin(0, 0, 0, 0),
+                                 legend.key.height = unit(2, units = "cm")))
 
-italy_legend <- get_legend(g_1 + theme(legend.position = "top", legend.box.margin = margin(0, 0, 0, 0)))
+g_maps_without_legend <- plot_grid(g_1 + theme(legend.position = "none"),
+                                   g_2 + theme(legend.position = "none"),
+                                   g_3 + theme(legend.position = "none"),
+                                   ncol = 1)
 
-g_maps <- plot_grid(g_1 + theme(legend.position = "none"),
-                    g_2 + theme(legend.position = "none"),
-                    g_3 + theme(legend.position = "none"),
+g_maps <- plot_grid(g_maps_without_legend,
                     italy_legend,
-                    ncol = 1,
-                    rel_heights = c(1,1,1,0.3))
+                    ncol = 2,
+                    rel_widths = c(1,0.5))
 
 plot_epi_df <- epi_df %>%
     group_by(date) %>%
@@ -65,7 +81,10 @@ plot_epi_df <- epi_df %>%
     mutate(date = as.Date(date, format = "%d-%m-%Y"))
 
 
-
+plot_mobility_df <- read.csv("../data-mobility/covid19db-mobility-GOOGLE_MOBILITY.csv", stringsAsFactors = FALSE) %>%
+    filter(country == "Italy", adm_area_1 == "") %>%
+    select(date, workplace) %>%
+    mutate(date = as.Date(date, format = "%d-%m-%Y"))
 
 plot_gov_df <- read.csv("../data-government-response/covid19db-government-response-GOVTRACK.csv", stringsAsFactors = FALSE) %>%
     filter(country == "Italy") %>%
@@ -73,6 +92,7 @@ plot_gov_df <- read.csv("../data-government-response/covid19db-government-respon
     mutate(date = as.Date(date, format = "%d-%m-%Y"))
 
 plot_ts_df <- left_join(plot_epi_df, plot_gov_df, by = "date") %>%
+    left_join(plot_mobility_df, by = "date") %>%
     melt(id.vars = "date")
 
 g_epi <- ggplot() +
@@ -80,23 +100,35 @@ g_epi <- ggplot() +
               mapping = aes(x = date, y = value)) +
     geom_vline(xintercept = as.Date(c(date_index_1, date_index_2, date_index_3), format = "%d-%m-%Y"),
                linetype = "dashed") +
-    labs(x = "Date", y = "Cumulative deaths")
+    labs(x = "Date", y = "Cumulative deaths") +
+    theme_classic()
+
 
 g_gov <- ggplot() +
-    geom_line(data = filter(plot_ts_df, variable == "stringency_indexfordisplay"),
+    geom_step(data = filter(plot_ts_df, variable == "stringency_indexfordisplay"),
               mapping = aes(x = date, y = value)) +
-    labs(x = "Date", y = "Intervention stringency")
+    labs(x = "Date", y = "Intervention stringency") +
+    theme_classic()
+
+g_mob <- ggplot() +
+    geom_step(data = filter(plot_ts_df, variable == "workplace"),
+              mapping = aes(x = date, y = value)) +
+    geom_hline(yintercept = 0, linetype = "dashed") +
+    labs(x = "Date", y = "Relative workplace mobility") +
+    theme_classic()
+
 
 g_ts <- plot_grid(g_epi,
                   g_gov,
+                  g_mob,
                   ncol = 1,
-                  labels = c("A", "B"))
+                  labels = c("A", "B", "C"))
 
 
 g_final <- plot_grid(g_ts,
                      g_maps,
                      ncol = 2,
-                     rel_widths = c(0.7,1),
-                     labels = c("","C"))
+                     rel_widths = c(0.7,1.0),
+                     labels = c("","D"))
 
 ggsave("out/demo-combination-plot.png", g_final, width = 2 * 14.8, height = 2 * 10.5, units = "cm")
